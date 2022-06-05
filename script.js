4a9c27c282a978396fc74f101d64f7aeedd19bfc
unknown <samueloctavianus97@gmail.com>
2022-06-05 14:37:28 +0700

    fixed dropdown menu functionality

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let btnMenu = document.querySelector(".header__menu");
 let sidenav = document.querySelector(".header__sidenav");
 let overlay = document.querySelector(".overlay");
+let dropdownParent = document.querySelectorAll(".dropdown-parent");
+let dropdownContent = document.querySelectorAll(".dropdown");
+let arrowIcon = document.querySelectorAll(".dropdown-parent::after");
+
+dropdownParent.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.classList.toggle("active");
+    element.nextElementSibling.classList.toggle("active");
+    console.log(element.nextElementSibling);
+  });
+});
+
+// function toggleDropdown(element) {
+//   console.log("clicked!");
+// }
 
 btnMenu.addEventListener("click", () => {
   sidenav.classList.toggle("active");
